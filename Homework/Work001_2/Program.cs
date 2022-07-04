505c9cda507e3392e32a87fc6e1b6c33aea2d3b5
Mikhail <rybalko_mikhail@bk.ru>
2022-07-04 13:07:29 +0300

    Work 001 2 Task 13

--- Homework/Work001_2/Program.cs
+++ Homework/Work001_2/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 13:
+Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+что третьей цифры нет.
+645 -> 5
+78 - третьей цифры нет
+32679 -> 6
+*/
+
+Console.WriteLine("Введите трехзначное число и больше");
+int number = int.Parse(Console.ReadLine());
+
+if(number > 99 && number < 1000)
+{
+    Console.WriteLine($"Третья цифра {number % 10}");
+}
+else if(number > 10000 && number < 100000)
+{
+    Console.WriteLine($"Третья цифра {number /100%10}");
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
